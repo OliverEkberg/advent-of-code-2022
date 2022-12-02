71260fd407f958090c3d6af5c9d3d2fa21903d7f
Oliver Ekberg <oliver.ekberg1889@gmail.com>
2022-12-02 07:09:51 +0100

    Day 2

---
+++ day02/ab.js
@@ -0,0 +1,61 @@
+const fs = require('node:fs')
+
+const rows = fs
+  .readFileSync('data.txt', 'utf8')
+  .split('\n')
+  .map((r) => r.split(' '))
+
+const ROCK = 'r'
+const PAPER = 'p'
+const SCISSORS = 's'
+
+const translate = {
+  A: ROCK,
+  X: ROCK,
+  B: PAPER,
+  Y: PAPER,
+  C: SCISSORS,
+  Z: SCISSORS,
+}
+
+const typeToPoints = {
+  [ROCK]: 1,
+  [PAPER]: 2,
+  [SCISSORS]: 3,
+}
+
+// input beats output
+const beats = {
+  [ROCK]: SCISSORS,
+  [PAPER]: ROCK,
+  [SCISSORS]: PAPER,
+}
+
+// reverse beats, needed for part b
+const looses = Object.fromEntries(Object.entries(beats).map((e) => e.reverse()))
+
+const calcScore = (strategy) => {
+  let total = 0
+  for (const [op, me] of strategy) {
+    // Points based on my selection
+    total += typeToPoints[me]
+
+    // Points based on outcome
+    if (me === op) total += 3
+    else if (beats[me] === op) total += 6
+    // else op win, 0 points
+  }
+
+  return total
+}
+
+const strategyA = rows.map(([op, me]) => [translate[op], translate[me]])
+
+const strategyB = strategyA.map(([op, me]) => {
+  if (me === ROCK) return [op, beats[op]] // loose
+  if (me === PAPER) return [op, op] // draw
+  if (me === SCISSORS) return [op, looses[op]] // win
+})
+
+console.log('A', calcScore(strategyA))
+console.log('B', calcScore(strategyB))
