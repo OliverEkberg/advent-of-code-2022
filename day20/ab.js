8451aaa5454c0c2b32b8235e46e41413ffb864dc
Oliver Ekberg <oliver.ekberg1889@gmail.com>
2022-12-20 07:45:51 +0100

    Day 20

---
+++ day20/ab.js
@@ -0,0 +1,45 @@
+const fs = require('node:fs')
+
+const rows = fs.readFileSync('data.txt', 'utf8').split('\n').map(Number)
+
+const mix = (numbers, times) => {
+  const lookupTable = new Map()
+
+  for (let i = 0; i < numbers.length; i++) {
+    lookupTable.set(i, numbers[i])
+  }
+
+  for (let round = 0; round < times; round++) {
+    for (let i = 0; i < rows.length; i++) {
+      const curr = lookupTable.get(i)
+
+      if (curr.value % rows.length === 0) continue
+
+      const idx = numbers.indexOf(curr)
+      numbers.splice(idx, 1)
+      numbers.splice((idx + curr.value) % numbers.length, 0, curr)
+    }
+  }
+}
+
+const sumGroove = (numbers) => {
+  let sum = 0
+  let startIdx = numbers.findIndex((v) => v.value === 0)
+
+  for (let i = 1; i <= 3000; i++) {
+    if (i % 1000 === 0) {
+      sum += numbers[(startIdx + i) % numbers.length].value
+    }
+  }
+
+  return sum
+}
+
+const aNumbers = rows.map((row) => ({ value: row }))
+const bNumbers = rows.map((row) => ({ value: row * 811589153 }))
+
+mix(aNumbers, 1)
+mix(bNumbers, 10)
+
+console.log('A', sumGroove(aNumbers))
+console.log('B', sumGroove(bNumbers))
